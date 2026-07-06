1cdf3e9b8e3b9b2b19a59e80c5ed713a44025b17
Farhad Ahmmed Turky <turkyfarhad@yahoo.com>
2026-07-06 19:04:54 +0600

    sign

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,43 @@
+'use client'
 import Navbar from "@/Components/Navbar";
+import { authClient } from "@/libs/auth-client";
+import { router } from "better-auth/api";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 
 const page = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = async(data) =>{
+        const { data:res, error } = await authClient.signIn.email({
+    email: data.email, 
+    password: data.password,
+    rememberMe: true,
+    callbackURL: "/",
+     
+});
+router.push("/");
+    }
     return (
-        <div>
+        <div className="px-3.5">
            
-            login
+           <div className="w-full md:w-[25%] md:pt-26 mx-auto py-25 px-20 bg-gray-100 space-y-4">
+              <h2 className="text-2xl font-semibold">Login here</h2>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-2" >
+              <fieldset className="fieldset">
+       <legend className="fieldset-legend">Email</legend>
+       <input type="email" {...register("email",{required:true})} className="input" placeholder="Type Your email" />
+      {errors.email&&<span>email is required</span>}
+     </fieldset>
+      <fieldset className="fieldset">
+      <legend className="fieldset-legend">password</legend>
+     <input type="password" {...register("password",{required:true})} className="input" placeholder="Type your password" />
+         {errors.password&&<span>password is required</span>}
+</fieldset>
+     <p>Don't Have any account ? <Link className="text-blue-600" href={'/signin'}>Click here </Link></p>
+            <button type="submit" className="btn w-full bg-[#403F3F] text-white">Login</button>
+            </form>
+           </div>
             
         </div>
     );
--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,9 +1,42 @@
-
-
+'use client'
+import Navbar from "@/Components/Navbar";
+import { authClient } from "@/libs/auth-client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 const page = () => {
+     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = async(data) =>{
+        const { data:res, error } = await authClient.signUp.email({
+   name:"turkyyyyy",
+    email:data.email, // required
+    password:data.password, // required
+    
+    callbackURL: "/",
+    
+});
+console.log(error)
+    }
     return (
-        <div>
-            signin
+         <div className="px-3.5">
+           
+           <div className="w-full md:w-[25%] md:pt-26 mx-auto py-25 px-20 bg-gray-100 space-y-4">
+              <h2 className="text-2xl font-semibold">Sign in here</h2>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-2" >
+              <fieldset className="fieldset">
+       <legend className="fieldset-legend">Email</legend>
+       <input type="email" {...register("email",{required:true})} className="input" placeholder="Type Your email" />
+      {errors.email&&<span>email is required</span>}
+     </fieldset>
+      <fieldset className="fieldset">
+      <legend className="fieldset-legend">password</legend>
+     <input type="password" {...register("password",{required:true})} className="input" placeholder="Type your password" />
+         {errors.password&&<span>password is required</span>}
+</fieldset>
+    
+            <button type="submit" className="btn w-full bg-[#403F3F] text-white">sign in </button>
+            </form>
+           </div>
+            
         </div>
     );
 };
